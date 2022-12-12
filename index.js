5d2a4ff7f973ab94e167397f2cd621a3cef753e7
pyague <pablo.y.g@hotmail.com>
2022-12-12 15:28:31 +0100

    deleted comments

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// globals
-const btnNumber = document.querySelectorAll('.number'); //select all the number keys
-const btnOperator =document.querySelectorAll('.operator'); //select all the number keys
+const btnNumber = document.querySelectorAll('.number'); 
+const btnOperator =document.querySelectorAll('.operator'); 
 const btnEqual = document.querySelector('.equal')
 const buttonClear = document.querySelector('.clear');
 const buttonDelete = document.querySelector('.delete')
@@ -9,10 +8,6 @@ const previousDisplay = document.querySelector('#previous-operation');
 let calc = {num1: "", num2: "", operator: "", solution: ""};
 let reset = false;
 
-// OPERATIONS
-// ----------
-
-// basic operators
 function add(x,y) {
     return x + y;
 }
@@ -26,7 +21,7 @@ function divide(x,y) {
     return (x === 0 ? "ERROR" : x / y);
 }
 
-// master operate function using calc object
+
 function operate() {
     switch (calc.operator) {
         case '+':  
@@ -42,20 +37,20 @@ function operate() {
     }
 }
 
-// add digit number to the display
+
 function updateDisplay(num) {
     if (reset){currentDisplay.innerHTML = ""};
-    if (num == "." && /\./.test(currentDisplay.innerHTML)) { //compara si tiene el punto ya puesto
+    if (num == "." && /\./.test(currentDisplay.innerHTML)) { 
         return;
     }
     currentDisplay.innerHTML += num;
-    reset = false; // turn off display reset
+    reset = false; 
 }
 
-// update displays when operator is selected
+
 function operationSelect(op) {
     if (calc.num1 && currentDisplay.innerHTML) {
-        calc.num1 = Number(currentDisplay.innerHTML);
+        calc.num2 = Number(currentDisplay.innerHTML);
         calc.num1 = operate();
         calc.operator = op;
         previousDisplay.innerHTML = calc.num1 + " " + op;
@@ -77,22 +72,18 @@ function completeCalc() {
             previousDisplay.innerHTML += " " + calc.num2
             currentDisplay.innerHTML = operate();
             calc = {};
-            reset = true; // toggle display reset
+            reset = true;
         }
 }
 
 
-// EVENT LISTENERS
-// ---------------
-
-// add event listeners for digit buttons
 btnNumber.forEach ((number) => {
     number.addEventListener('click' , function() {
         updateDisplay(parseFloat(number.value));
     });
 });
 
-// add event listeners for operator buttons
+
 
 btnOperator.forEach((operator) => {
     operator.addEventListener('click', function() {
@@ -100,14 +91,13 @@ btnOperator.forEach((operator) => {
     })
 });
 
-// event listener for equals button
+
 btnEqual.addEventListener('click', function() {
     completeCalc();
 });
 
 
-// CLEAR FUNCTIONS
-// -----------------
+
 function clearAll() {
     previousDisplay.innerHTML = "" ;
     currentDisplay.innerHTML = "";
@@ -115,24 +105,28 @@ function clearAll() {
     operation = "";
 }
 
-//clear all button press
-
 buttonClear.addEventListener('click', function(){
     clearAll();
  })
 
 
-
 function backspace() {
     currentDisplay.innerHTML = currentDisplay.innerHTML.slice(0, -1);
 }
-//Delete button press
+
 buttonDelete.addEventListener('click', function() {
    backspace();
 });
 
+function roundDecimal(num, decimal) {
+    numRegexp = new RegExp('\\d\\.(\\d){' + decimal + ',}');   
+    if (numRegexp.test(num)) {         
+        return Number(num.toFixed(decimal));
+    } else {
+        return Number(num.toFixed(decimal)) === 0 ? 0 : num;  
+    }
+}
 
-// keyboard support
 document.addEventListener('keyup', e => {
     switch (e.key) {
         case "Backspace":
@@ -166,11 +160,3 @@ document.addEventListener('keyup', e => {
     }
 })
 
-function roundDecimal(numero, decimales) {
-    numeroRegexp = new RegExp('\\d\\.(\\d){' + decimales + ',}');   // Expresion regular para numeros con un cierto numero de decimales o mas
-    if (numeroRegexp.test(numero)) {         // Ya que el numero tiene el numero de decimales requeridos o mas, se realiza el redondeo
-        return Number(numero.toFixed(decimales));
-    } else {
-        return Number(numero.toFixed(decimales)) === 0 ? 0 : numero;  // En valores muy bajos, se comprueba si el numero es 0 (con el redondeo deseado), si no lo es se devuelve el numero otra vez.
-    }
-}
